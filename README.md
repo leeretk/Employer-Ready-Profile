cecefda4477a053d2e2bd97bc9543601f85770a3
Kamy <kamy.leeret@gmail.com>
2020-03-28 21:58:30 -0600

    Initial Submit
    Messing with CSS & Bootstrap

--- README.md
+++ README.md
@@ -2,3 +2,31 @@
 Employer-Ready-Portfolio
 
 Profile for Kamy Leeret
+
+- [] Resume
+
+Projects: 
+- [] Giftastic
+- [] Train-Schedule
+- [] Trails on Tap
+
+Sources:
+- [] https://www.gstatic.com/firebasejs/6.0.4/firebase-app.js
+- [] https://www.gstatic.com/firebasejs/6.0.4/firebase-database.js
+- [] https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
+- [] assets/javascript/app.js
+- [] https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js
+- [] https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js
+
+
+css:
+- [] https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css
+- [] assets\css\style.css
+- [] js/materialize.min.js"
+
+
+Color Scheme: 
+-[] #004d40 teal darken-4
+-[] #616161 grey darken-2
+-[] #37474f blue-grey darken-3
+-[] #bcaaa4 brown lighten-3
---
+++ assets/js/contacts.js
@@ -0,0 +1,92 @@
+
+var config = {
+        apiKey: "AIzaSyBxhsSVuUdpKqx6EQXdwGupeOFyss6D3YU",
+        authDomain: "portfolio-contacts-599f4.firebaseapp.com",
+        databaseURL: "https://portfolio-contacts-599f4.firebaseio.com",
+        projectId: "portfolio-contacts-599f4",
+        storageBucket: "portfolio-contacts-599f4.appspot.com",
+        messagingSenderId: "810458093542",
+        appId: "1:810458093542:web:22e37d66c4fc5fc9afe827"
+  };
+
+  firebase.initializeApp(config);
+  var database = firebase.database();
+  
+var name = 0;
+var email = 0;
+var message = "";
+var date=0;
+
+///// ADD RECORDS TO THE DATABASE //////
+
+$("#add-new-contact").on("click", function () {
+  event.preventDefault();
+  
+  clickCounter++;
+  console.log()
+
+  //grabs user input//
+  clickCounter = $("#counter-input").val().trim();
+  name = $("#name-input").val().trim();
+  email = $("#email-input").val().trim();
+  message = $("#message-input").val().trim();
+  date=$("#date-input").val().trim();
+
+  //creates local "temporary" object for holding train data//
+  var contactPush = {
+    clickCounter: clickCounter,
+    name: name,
+    email: email,
+    message: message,
+    date: date,
+    };
+
+  //uploads data to the database
+  database.ref().push(trainPush);
+
+  console.log("got a Lead" + contactPush);
+  console.log(contactPush.name);
+  console.log(contactPush.email);
+  console.log(contactPush.message);
+  console.log(contactPush.date);
+
+ // Clears all of the text-boxes
+  $("#counter-input").val("");
+  $("#name-input").val("");
+  $("#email-input").val("");
+  $("#message-input").val("");
+  $("#date-input").val("");
+  
+});
+
+//create a firebase event
+
+database.ref().on("child_added", function(childSnapshot){
+  console.log(childSnapshot.val());
+  var contactCounter=childSnapshot.val().clickCounter;
+  var contactName =childSnapshot.val().name;
+  var contactEmail =childSnapshot.val().email;
+  var contactMessage =childSnapshot.val().message;
+  var contactDate =childSnapshot.val().date;
+
+ // Prettify Date
+
+ var contactDatePretty = moment.unix(contactDate).format('LLLL');
+
+//  var trainMinutesAwayPretty = moment.unix(trainMinutesAway).format(":mm");
+//  var trainFrequencyPretty = moment.unix(trainFrequency).format(":mm");
+
+  // Create the new row
+  var newRow = $("<tr>").append(
+    $("<td>").text(contactCounter),
+    $("<td>").text(contactEmail),
+    $("<td>").text(contactMessage),
+    $("<td>").text(contactName),
+    $("<td>").text(contactDatePretty),
+  );
+
+ // Append the new row to the table
+ $("#contact-table > tbody").append(newRow);
+
+});
+
