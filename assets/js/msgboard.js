52457da5653c66deaaad96ac39679658aef767e3
Kamy <kamy.leeret@gmail.com>
2020-04-02 22:16:55 -0600

    trying to get js to work

--- assets/js/msgboard.js
+++ assets/js/msgboard.js
@@ -1,36 +1,41 @@
 
-var config = {
-        apiKey: "AIzaSyBxhsSVuUdpKqx6EQXdwGupeOFyss6D3YU",
-        authDomain: "portfolio-contacts-599f4.firebaseapp.com",
-        databaseURL: "https://portfolio-contacts-599f4.firebaseio.com",
-        projectId: "portfolio-contacts-599f4",
-        storageBucket: "portfolio-contacts-599f4.appspot.com",
-        messagingSenderId: "810458093542",
-        appId: "1:810458093542:web:22e37d66c4fc5fc9afe827"
-  };
+var firebaseConfig = {
+  apiKey: "AIzaSyBxhsSVuUdpKqx6EQXdwGupeOFyss6D3YU",
+  authDomain: "portfolio-contacts-599f4.firebaseapp.com",
+  databaseURL: "https://portfolio-contacts-599f4.firebaseio.com",
+  projectId: "portfolio-contacts-599f4",
+  storageBucket: "portfolio-contacts-599f4.appspot.com",
+  messagingSenderId: "810458093542",
+  appId: "1:810458093542:web:22e37d66c4fc5fc9afe827"
+};
 
-  firebase.initializeApp(config);
-  var database = firebase.database();
-  
+firebase.initializeApp(config);
+var portoflioContacts = firebase.database();
+
+///// ADD RECORDS TO THE FIREBASE DATABASE //////
+
+//set variables//
+var clickCounter = 0;
 var name = 0;
 var email = 0;
 var message = "";
-var date=0;
+var date = 0;
 
 ///// ADD RECORDS TO THE DATABASE //////
 
-$("#add-new-contact").on("click", function () {
+$("#add-new-contact-btn").on("click", function () {
   event.preventDefault();
-  
+
   clickCounter++;
-  console.log()
+  console.log(clickCounter)
+  console.log();
 
   //grabs user input//
   // clickCounter = $("#counter-input").val().trim();
   name = $("#name-input").val().trim();
   email = $("#email-input").val().trim();
   message = $("#message-input").val().trim();
-  date=$("#date-input").val().trim();
+  date = $("#date-input").val().trim();
 
   //creates local "temporary" object for holding train data//
   var contactPush = {
@@ -39,53 +44,54 @@ $("#add-new-contact").on("click", function () {
     email: email,
     message: message,
     date: date,
-    };
+  };
 
   //uploads data to the database
-  database.ref().push(trainPush);
+  portoflioContacts.ref().push(contactPush);
 
   console.log("got a Lead" + contactPush);
+
   console.log(contactPush.name);
+  console.log(clickCounter);
   console.log(contactPush.email);
   console.log(contactPush.message);
   console.log(contactPush.date);
 
- // Clears all of the text-boxes
-  $("#counter-input").val("");
+  // Alert
+  alert("Message Added");
+
+  // Clears all of the text-boxes
   $("#name-input").val("");
   $("#email-input").val("");
   $("#message-input").val("");
   $("#date-input").val("");
-  
+console.log("data received")
 });
+
+
+///// CREATE A FIREBASE EVENT //////
 
 //create a firebase event
 
-database.ref().on("child_added", function(childSnapshot){
+database.ref().on("child_added", function (childSnapshot) {
   console.log(childSnapshot.val());
-  var contactCounter=childSnapshot.val().clickCounter;
-  var contactName =childSnapshot.val().name;
-  var contactEmail =childSnapshot.val().email;
-  var contactMessage =childSnapshot.val().message;
-  var contactDate =childSnapshot.val().date;
 
- // Prettify Date
+  //store everything into a variable
+  var contactCounter = childSnapshot.val().clickCounter;
+  var contactName = childSnapshot.val().name;
+  var contactEmail = childSnapshot.val().email;
+  var contactMessage = childSnapshot.val().message;
+  var contactDate = childSnapshot.val().date;
 
- var contactDatePretty = moment.unix(contactDate).format('LLLL');
+  console.log("Copied to Database" + contactCounter);
 
-//  var trainMinutesAwayPretty = moment.unix(trainMinutesAway).format(":mm");
-//  var trainFrequencyPretty = moment.unix(trainFrequency).format(":mm");
-
-  // Create the new row
-  var newRow = $("<tr>").append(
-    $("<td>").text(contactCounter),
-    $("<td>").text(contactEmail),
-    $("<td>").text(contactMessage),
-    $("<td>").text(contactName),
-    $("<td>").text(contactDatePretty),
+  // Add each contacts's data into the table
+  $("#contacts-table > tbody").append(
+    $("<tr>").append(
+      $("<td>").text(contactName),
+      $("<td>").text(contactEmail),
+      $("<td>").text(contactMessage),
+      $("<td>").text(contactDate),
+    )
   );
-
- // Append the new row to the table
- $("#contact-table > tbody").append(newRow);
-
 });
